5f41564aa964f67d7f2a367dcccd4752a374172a
Juan Caspi <juan.caspi7@gmail.com>
2020-05-17 17:15:51 -0500

    Configuración de servidor express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log('Hola mundo en el curso de GraphQL!!! :D')
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(compression());
+
+app.use("/", (re, res) => {
+  res.send("Bienvenido a la app de GraphQL");
+});
+
+const PORT = "5300";
+
+app.listen({ port: PORT }, () =>
+  console.log(
+    `Hola mundo api GraphQL ejecutando por la url http://localhost:${PORT}`
+  )
+);

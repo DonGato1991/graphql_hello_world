086e161733910d09614130c053b1ac56cf774de5
Juan Caspi <juan.caspi7@gmail.com>
2020-05-17 19:23:23 -0500

    Refactor using Apollo Server Express

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,39 @@
-import express from 'express';
-import compression from 'compression';
-import cors from 'cors';
-import GraphQLHTTP from 'express-graphql';
-import schema from './schema';
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+import schema from "./schema";
+import { ApolloServer } from "apollo-server-express";
+import { createServer } from "http";
 
 const app = express();
 
-app.use('*', cors());
+app.use("*", cors());
 
 app.use(compression());
 
-const PORT = '5300';
+const PORT = "5300";
 
+const server = new ApolloServer({
+  schema,
+  introspection: true,
+});
+server.applyMiddleware({ app });
+
+const httpServer = createServer(app);
+httpServer.listen({ port: PORT }, () =>
+  console.log(
+    `Hola mundo api GraphQL ejecutando por la url http://localhost:${PORT}/graphql`
+  )
+);
+
+/*
 app.use('/',GraphQLHTTP({
     schema ,
     graphiql: true
 }));
-
-app.listen(
-    { port: PORT},
-    ()=>console.log(`Hola mundo api GraphQL ejecutando por la url http://localhost:${PORT}/graphql`)
+app.listen({ port: PORT }, () =>
+  console.log(
+    `Hola mundo api GraphQL ejecutando por la url http://localhost:${PORT}/graphql`
+  )
 );
+*/
